2726da09283f6819124bd211e76c0d18414b126d
Gabriel LEPETIT-AIMON <gabriel.lepetit-aimon@polymtl.ca>
2023-03-11 20:50:07 -0500

    Add Pypi project file.

--- setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-pip install --no-index --upgrade pip
-
-pip install numpy torch torchvision pytorch-lightning torchmetrics wandb albumentations opencv-python-headless pyyaml segmentation-models-pytorch parse optuna webcolors ttach -U
